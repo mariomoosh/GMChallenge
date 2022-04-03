1a255bb64faa1c85bf8a58bf63432d2f07d533bf
mariomoosh <mherrera820903@gmail.com>
2022-04-03 17:49:47 -0500

    foundation ready

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>GM Coding Challenge</Text>
       <StatusBar style="auto" />
     </View>
   );
