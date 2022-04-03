32e52c8c275c939dd118cd0edd3aff7c4fa8ed43
mariomoosh <mherrera820903@gmail.com>
2022-04-03 18:13:08 -0500

    mount to simulator complete, up and running

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { SafeAreaView, FlatList, StyleSheet, Text, View } from 'react-native';
 
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 export default function App() {
+  const [commits,setCommits] = useState([
+    {
+      id: '1',
+      title: 'First Item',
+    },
+    {
+      id: '2',
+      title: 'Second Item',
+    },
+    {
+      id: '3',
+      title: 'Third Item',
+    },
+  ])
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
   return (
-    <View style={styles.container}>
-      <Text>GM Coding Challenge</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>GM Coding Challenge</Text>
+      <FlatList
+        data={commits}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight || 0,
   },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  header:{
+    fontSize:40,
+    fontWeight: 'bold'
+  }
 });
